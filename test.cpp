0f7b524dce5ecda167f7cc04913bfdbc10a7ef55
michael07w <michaelwessels207@gmail.com>
2021-11-09 21:19:38 -0800

    Implemented print_selection() function in spreadsheet.cpp. Implemented basic tests to confirm function printed all rows of spreadsheet when select is null pointer. Will add more tests when SELECT_* classes are implemented. Confirmed print_selection() passes all created tests. Confirmed entire spreadsheet will print 4 times when main is run, as it should.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,32 @@
-#include "spreadsheet.hpp"
+#include "spreadsheet.cpp"
 
 #include "gtest/gtest.h"
 
+#include <sstream>
+
+TEST(PrintSelectionTest, NonEmptySheetWithNullPointer) {
+    std::ostringstream sstream;
+    Spreadsheet sheet;
+    sheet.set_column_names({"First", "Last", "Age", "Major"});
+    sheet.add_row({"Joe", "Schmo", "22", "mathematics"});
+    sheet.add_row({"Michael", "Wessels", "23", "csba"});
+
+    std::string expected_val = "Joe Schmo 22 mathematics \nMichael Wessels 23 csba \n";
+    sheet.print_selection(sstream);
+    EXPECT_TRUE(sstream.str() == expected_val);
+}
+
+TEST(PrintSelectionTest, EmptySheetWithNullPointer) {
+    std::ostringstream sstream;
+    Spreadsheet sheet;
+    sheet.set_column_names({"First", "Last", "Age", "Major"});
+
+    std::string expected_val = "";
+    sheet.print_selection(sstream);
+    EXPECT_TRUE(sstream.str() == expected_val);
+}
+
+// Will add further tests for print_selection() once SELECT_* classes are implemented
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
